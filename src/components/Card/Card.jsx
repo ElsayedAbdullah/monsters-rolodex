406f06db31e59739dc9846052e6238055e42a6f0
Elsayed Abdullah <sayedaladway@gmail.com>
2022-08-22 14:21:07 +0200

    adding some styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import "./Card.css";
 
 const Card = (props) => (
   <div className="card-container">
-    <img src={`https://robohash.org/${props.monster.id}.png?set=set2`} alt="" />
+    <img src={`https://robohash.org/${props.monster.id}.png?set=set2&size=250x250`} alt="" />
     <h2>{props.monster.name}</h2>
     <p>{props.monster.email}</p>
   </div>
